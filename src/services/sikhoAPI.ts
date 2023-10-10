45bd7ae796c2b80f09b2accbe17fcd2562854d02
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-10-09 20:17:58 -0700

    changed api path

--- src/services/sikhoAPI.ts
+++ src/services/sikhoAPI.ts
@@ -6,14 +6,14 @@ const client = axios.create({
 
 // Industry
 export const getIndustries = async () => {
-  const response = await client.get('industry');
+  const response = await client.get('/industry');
   console.log('here are the industriessss ', response);
   return response.data;
 };
 
 // Occupations
 export const getOccupations = async (industryId?: string) => {
-  const response = await client.get('occupation', {
+  const response = await client.get('/occupation', {
     params: { industryId },
   });
   return response.data;
@@ -22,25 +22,25 @@ export const getOccupations = async (industryId?: string) => {
 // Company
 
 export const getCompanies = async () => {
-  const response = await client.get('company');
+  const response = await client.get('/company');
   return response.data;
 };
 
 // Datapoints
 
 export const getDatapoints = async () => {
-  const response = await client.get('datapoint');
+  const response = await client.get('/datapoint');
   return response.data;
 };
 
 // JobGrowthPolls
 
 export const postJobGrowthPoll = async (poll: any) => {
-  const response = await client.post('jobgrowthpoll', poll);
+  const response = await client.post('/jobgrowthpoll', poll);
   return response.data;
 };
 
 export const getJobGrowthPoll = async () => {
-  const response = await client.get('jobgrowthpoll');
+  const response = await client.get('/jobgrowthpoll');
   return response.data;
 };
